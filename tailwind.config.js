c54e03e9ab3ad9da52dc7e513dfb56752e06e7ba
Vishwas <thangellavishwas@gmail.com>
2024-03-01 23:15:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        bgblue:'#172554',
+        tomato:'tomato'
+      }
+    },
+  },
+  plugins: [],
+}
+
